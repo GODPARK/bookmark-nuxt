767bc1e5f3f3be9fb0b557fa634cedb37c294f48
Jung Hyun Park <rootjh937@gmail.com>
2021-04-25 12:28:54 +0900

    서버 포트 80 변경

--- nuxt.config.js
+++ nuxt.config.js
@@ -3,7 +3,7 @@ import colors from 'vuetify/es5/util/colors'
 export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
   server: {
-    port: 8080,
+    port: 80,
     host: '0.0.0.0'
   },
   head: {
